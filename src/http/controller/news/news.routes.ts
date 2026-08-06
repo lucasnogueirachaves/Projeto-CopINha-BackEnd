1d97ee143017d8cc1dcd7cfc6567e468935559d6
Henrique Veloso <@henriquevcm07@gmail.com>
2026-08-06 17:00:07 -0300

    feat: rotas de notícias

---
+++ src/http/controller/news/news.routes.ts
@@ -0,0 +1,11 @@
+import { createNews } from './createNews.js'
+import { getNews } from './getNews.js'
+import { listRelatedNews } from './listRelatedNews.js'
+import { listNews } from './listNews.js'
+
+export function newsRoutes(app: any) {
+    app.get('/news', listNews)
+    app.get('/news/:id/related', listRelatedNews)
+    app.get('/news/:id', getNews)
+    app.post('/news', createNews)
+}
