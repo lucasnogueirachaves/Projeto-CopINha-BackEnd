8003111e2f09297770ea91e9ed46732c7f5706e8
Henrique Veloso <@henriquevcm07@gmail.com>
2026-08-06 14:38:20 -0300

    feat: criando rota HTTP para o login

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import fastify from "fastify"
-import fastifyJwt from "@fastify/jwt"   
+import fastifyJwt from "@fastify/jwt"
 import { env } from "./env/index.js"
+import { routes } from "./http/controller/routes.js"
 
 export const app = fastify()
+
+app.register(routes)
 
 app.register(fastifyJwt, {
     secret: env.JWT_SECRET
