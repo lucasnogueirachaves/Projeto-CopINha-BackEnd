b3940c4df3a844e185499f854f9b04620b3ff4e0
Henrique Veloso <@henriquevcm07@gmail.com>
2026-08-06 17:05:55 -0300

    feat: adicionando checagem de token

--- src/http/controller/news/news.routes.ts
+++ src/http/controller/news/news.routes.ts
@@ -2,10 +2,11 @@ import { createNews } from './createNews.js'
 import { getNews } from './getNews.js'
 import { listRelatedNews } from './listRelatedNews.js'
 import { listNews } from './listNews.js'
+import { verifyJwt } from '../middleware/verify-jwt.js'
 
 export function newsRoutes(app: any) {
     app.get('/news', listNews)
     app.get('/news/:id/related', listRelatedNews)
     app.get('/news/:id', getNews)
-    app.post('/news', createNews)
+    app.post('/news',{onRequest: verifyJwt}, createNews)
 }
