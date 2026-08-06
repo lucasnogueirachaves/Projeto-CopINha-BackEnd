cfce0a8733b2aa88039b9a16a429705344695e92
Henrique Veloso <@henriquevcm07@gmail.com>
2026-08-06 16:23:48 -0300

    feat: adicionando listagem de noticias por ordem de criação

---
+++ src/http/controller/news/listNews.ts
@@ -0,0 +1,30 @@
+import { z } from 'zod'
+import { prisma } from '@/libs/prisma.js'
+import type { FastifyReply, FastifyRequest } from 'fastify'
+
+export async function listNews(request: FastifyRequest, reply: FastifyReply) {
+    const listNewsQuerySchema = z.object({
+        page: z.number().optional().default(1),
+        })
+    const { page } = listNewsQuerySchema.parse(request.query)
+    const perPage = 5
+    const skip = (page - 1) * perPage
+    
+    const news = await prisma.news.findMany({
+        take: perPage,
+        skip: skip,
+        orderBy: {
+            createdAt: 'desc'
+        },
+        include: {
+            author: true,
+            group: true,
+            title: true,
+            readingTime: true,
+            summary: true,
+            createdAt: true,
+            image: true
+        }
+    })
+    return reply.status(200).send(news)
+}
