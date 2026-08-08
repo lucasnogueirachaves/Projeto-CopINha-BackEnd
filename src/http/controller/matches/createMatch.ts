b7d8d2dd06e891e251ceff9272804704e4809177
Lucas Nogueira <lucasnogueirachaves@id.uff.br>
2026-08-08 00:40:10 -0300

    test

--- src/http/controller/matches/createMatch.ts
+++ src/http/controller/matches/createMatch.ts
@@ -2,9 +2,12 @@ import { z } from 'zod'
 import { prisma } from '@/libs/prisma.js'
 import type { FastifyReply, FastifyRequest } from 'fastify'
 
-export async function createMatch(request: FastifyRequest, reply: FastifyReply) {
+export async function createMatch(
+    request: FastifyRequest,
+    reply: FastifyReply
+) {
     const createGameBodySchema = z.object({
-        date: z.date(),
+        date: z.coerce.date(),
         local: z.string(),
         homeTeamId: z.coerce.number(),
         awayTeamId: z.coerce.number(),
@@ -12,29 +15,39 @@ export async function createMatch(request: FastifyRequest, reply: FastifyReply) 
         status: z.enum(['ENCERRADO', 'PROXIMO']),
     })
 
-    const { date, local, homeTeamId, awayTeamId, groupId, status } = createGameBodySchema.parse(request.body)
+    const {
+        date,
+        local,
+        homeTeamId,
+        awayTeamId,
+        groupId,
+        status
+    } = createGameBodySchema.parse(request.body)
 
     const groupAwayTeam = await prisma.team.findUnique({
         where: { id: awayTeamId },
         select: { groupId: true }
     })
+
     const groupHomeTeam = await prisma.team.findUnique({
         where: { id: homeTeamId },
         select: { groupId: true }
     })
 
-    const IsSameGroup = groupAwayTeam?.groupId === groupHomeTeam?.groupId && groupHomeTeam?.groupId === groupId
+    const isSameGroup =
+        groupAwayTeam?.groupId === groupHomeTeam?.groupId &&
+        groupHomeTeam?.groupId === groupId
 
-    if (!IsSameGroup) {
-        return reply.status(400).send({ message: 'Os times devem pertencer ao mesmo grupo referente a partida' })
+    if (!isSameGroup) {
+        return reply.status(400).send({
+            message: 'Os times devem pertencer ao mesmo grupo referente à partida'
+        })
     }
 
     if (homeTeamId === awayTeamId) {
-        return reply.status(400).send({ message: 'Os times não podem ser iguais' })
-    }
-
-    if (!date || !local || !homeTeamId || !awayTeamId || !groupId || !status) {
-        return reply.status(400).send({ message: 'campos ausentes' })
+        return reply.status(400).send({
+            message: 'Os times não podem ser iguais'
+        })
     }
 
     const match = await prisma.match.create({
@@ -47,5 +60,6 @@ export async function createMatch(request: FastifyRequest, reply: FastifyReply) 
             status
         }
     })
+
     return reply.status(201).send({ match })
 }
